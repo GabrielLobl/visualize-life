db9c9714d866accc20d3c94de7d2980e62d28805
Gabriel Lobl <gabolobl@yahoo.com>
2020-08-31 01:11:40 -0400

    Updates

---
+++ precache-manifest.42b4ae80978d1e3795cca8a718863cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4c796638ec7749bd2a3b6a316cd3c1",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "ff64bdecab974fd5e930",
+    "url": "/life/static/css/main.2efaa9b6.chunk.css"
+  },
+  {
+    "revision": "2e52679bcf722e4ce205",
+    "url": "/life/static/js/2.eee647bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/life/static/js/2.eee647bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff64bdecab974fd5e930",
+    "url": "/life/static/js/main.87d7a028.chunk.js"
+  },
+  {
+    "revision": "42a87418d77f7755616f",
+    "url": "/life/static/js/runtime-main.23056208.js"
+  }
+]);
